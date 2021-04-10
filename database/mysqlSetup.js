d1bb67bb87bf915c6094b81b885c3e70ffd597ff
Andrew Song <andrewsong61@gmail.com>
2021-04-10 12:12:47 -0700

    Setup mysql connection

---
+++ database/mysqlSetup.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'townhall',
+  database: 'pokemon'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ', err);
+    return;
+  }
+
+  console.log('Connected to MySQL'); 
+});
+
+module.exports.connection = connection;
