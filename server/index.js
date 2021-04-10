0696d37ade88e05e766375915e725a36a5408fbc
Andrew Song <andrewsong61@gmail.com>
2021-04-10 12:29:37 -0700

    Add TODO instructions

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 // const db = require('../database/mysqlSetup'); /* MYSQL CONNECTION */
 // const { addToPokedex, getAllPokemon } = require('../database/mongoSetup'); /* MONGODB CONNECTION */
 
+const exampleData = require('../exampleData.json');
+
 const app = express();
 const PORT = 3000;
 
@@ -10,18 +12,26 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.post('/mysqlPokemon', (req, res) => {
+  // TODO: Add a POST request handler that will 
+  // add a new pokemon to our MySQL "Pokedex"
 
 });
 
 app.get('/mysqlPokemon', (req, res) => {
+  // TODO: Add a GET request handler that will 
+  // fetch all pokemon from MySQL
 
 });
 
 app.post('/mongoosePokemon', (req, res) => {
+  // TODO: Add POST request handler that will 
+  // add a new pokemon to our MongoDB "Pokedex"
 
 });
 
 app.get('/mongoosePokemon', (req, res) => {
+  // TODO: Add POST request handler that will 
+  // fetch all pokemon from MongoDB
 
 })
 
