d178e4053596df239f534c5c903cb5c82b0ce198
Andrew Song <andrewsong61@gmail.com>
2021-04-10 12:33:46 -0700

    Add solution

--- database/mongoSetup.js
+++ database/mongoSetup.js
@@ -13,12 +13,28 @@ let Pokemon = mongoose.model('Pokemon', pokemonSchema);
 let addToPokedex = (pokemon, callback) => {
   // TODO: This function should take one or more pokemon 
   // and save this into our database
-  
+
+  Pokemon.create(pokemon)
+    .then((results) => {
+      callback(null, results);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+
 };
 
 let getAllPokemon = (callback) => {
   // TODO: This function should fetch all pokemon in our database
 
+  Pokemon.find({}, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, results);
+    }
+  });
+  
 };
 
 module.exports = {
