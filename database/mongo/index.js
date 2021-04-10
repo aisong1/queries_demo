d98487bfbe862c325a7f504c550fa616137a80e3
Andrew Song <andrewsong61@gmail.com>
2021-04-10 14:55:29 -0700

    Move to separate directories and rename

---
+++ database/mongo/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pokemon');
+
+/* Insert feedback loop here */
+
+let pokemonSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  type: String,
+  trait: String
+});
+
+let Pokemon = mongoose.model('Pokemon', pokemonSchema);
+
+let addToPokedex = (pokemon, callback) => {
+  // TODO: This function should take one or more pokemon 
+  // and save this into our database
+  
+  Pokemon.create(pokemon)
+    .then((results) => {
+      callback(null, results);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+
+};
+
+let getAllPokemon = (callback) => {
+  // TODO: This function should fetch all pokemon in our database
+
+  Pokemon.find({}, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, results);
+    }
+  })
+
+};
+
+module.exports = {
+  addToPokedex,
+  getAllPokemon
+}
---
+++ database/mysql/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS pokemon;
+
+CREATE DATABASE pokemon;
+USE pokemon;
+
+DROP TABLE IF EXISTS pokemon;
+
+CREATE TABLE pokemon (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  trait VARCHAR(255) NOT NULL
+);
+
+-- Execute this file using mysql -u root -p < database/mysql/schema.sql
